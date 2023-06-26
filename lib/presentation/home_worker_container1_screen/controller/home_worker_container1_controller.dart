4c27aca904b0265ac00b31b9154b1512207a6577
Dharmik Ratanpara <dharmikratanpara25@gmail.com>
2023-06-26 18:29:01 +0530

    Firebase configuration added

--- lib/presentation/home_worker_container1_screen/controller/home_worker_container1_controller.dart
+++ lib/presentation/home_worker_container1_screen/controller/home_worker_container1_controller.dart
@@ -1,6 +1,5 @@
 import 'package:clean_city_worker/core/app_export.dart';
 import 'package:clean_city_worker/presentation/home_worker_container1_screen/models/home_worker_container1_model.dart';
-import 'package:clean_city_worker/widgets/custom_bottom_bar.dart';
 
 class HomeWorkerContainer1Controller extends GetxController {
   Rx<HomeWorkerContainer1Model> homeWorkerContainer1ModelObj =
